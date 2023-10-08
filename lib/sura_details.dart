c0ff113345a1fa7e318d87212d2bafcf5359ca79
AbdelhadyMohamed <86628356+AbdelhadyMohamed@users.noreply.github.com>
2023-10-08 12:50:28 +0300

    sebha added with no bugs

--- lib/sura_details.dart
+++ lib/sura_details.dart
@@ -62,7 +62,7 @@ class _SuraDetailsState extends State<SuraDetails> {
     );
   }
 
-  void loadFile(int index) async {
+  Future<void> loadFile(int index) async {
     String sura = await rootBundle.loadString("assets/files/${index + 1}.txt");
     List<String> lines = sura.split("\n");
     verses = lines;
--- lib/tabs/sebha.dart
+++ lib/tabs/sebha.dart
@@ -1,12 +1,85 @@
 import 'package:flutter/material.dart';
+import 'package:islami_app/my_theme_data.dart';
 
-class SebhaTab extends StatelessWidget {
-  const SebhaTab({super.key});
+class SebhaTab extends StatefulWidget {
+  SebhaTab({super.key});
+
+  @override
+  State<SebhaTab> createState() => _SebhaTabState();
+}
+
+class _SebhaTabState extends State<SebhaTab> {
+  List<String> sebhaList = [
+    "سبحان الله",
+    "لا اله الا الله",
+    "الله اكبر",
+    "الحمدلله"
+  ];
+
+  int counter = 0;
+  int index = 0;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.amber,
+    return Center(
+      child: Column(
+        children: [
+          Image.asset("assets/images/Group 7.png"),
+          SizedBox(
+            height: 35,
+          ),
+          Text(
+            "عدد التسبيحات",
+            style: Theme.of(context).textTheme.bodyMedium,
+          ),
+          Container(
+            decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(15),
+                color: Color.fromRGBO(183, 147, 95, 100)),
+            height: 81,
+            width: 69,
+            child: Center(
+              child: Text(
+                "$counter",
+                textAlign: TextAlign.center,
+                style: Theme.of(context).textTheme.bodyMedium,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+          Container(
+            height: 51,
+            width: 137,
+            child: ElevatedButton(
+              onPressed: () {
+                counter++;
+                setState(() {});
+                if (counter == 30) {
+                  if (index == sebhaList.length - 1) {
+                    counter = 0;
+                    index = 0;
+                  } else {
+                    counter = 0;
+                    index++;
+                  }
+                }
+              },
+              child: Text(
+                sebhaList[index],
+                style: Theme.of(context)
+                    .textTheme
+                    .bodySmall!
+                    .copyWith(color: Colors.white),
+              ),
+              style: ElevatedButton.styleFrom(
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                backgroundColor: MyThemeData.primaryColor,
+              ),
+            ),
+          ),
+        ],
+      ),
     );
   }
 }
